594bc00f852c4f2ce870a95de934176eeda70c1a
graemerocher <graeme.rocher@gmail.com>
2020-06-03 12:53:16 +0200

    Update build / dependencies

--- rxjava3/src/main/java/io/micronaut/rxjava3/instrument/RxInstrumentedFlowable.java
+++ rxjava3/src/main/java/io/micronaut/rxjava3/instrument/RxInstrumentedFlowable.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -61,3 +61,4 @@ final class RxInstrumentedFlowable<T> extends Flowable<T> implements RxInstrumen
         }
     }
 }
+
