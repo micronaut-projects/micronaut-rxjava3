8bd13cfd4d823cd0d46db9a93f6b025b826ee71b
sdelamo <sdelamo@users.noreply.github.com>
2023-07-13 15:26:20 +0000

    Deploying to gh-pages - 15:26:20

---
+++ 3.0.1/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.rxjava3.converters"},{"l":"io.micronaut.rxjava3.http.client"},{"l":"io.micronaut.rxjava3.http.client.proxy"},{"l":"io.micronaut.rxjava3.http.client.sse"},{"l":"io.micronaut.rxjava3.http.client.websockets"}];updateSearchResults();
